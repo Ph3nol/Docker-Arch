ba93f6c5cd144d0332abaa93c546aaadfbf105b3
Cédric Dugat <cedric@dugat.me>
2017-09-25 08:24:14 +0000

    ElasticSearch data location customization added

--- src/Application/DockerContainer/ElasticsearchDockerContainer.php
+++ src/Application/DockerContainer/ElasticsearchDockerContainer.php
@@ -2,6 +2,7 @@
 
 namespace Ph3\DockerArch\Application\DockerContainer;
 
+use Ph3\DockerArch\Application\Architect;
 use Ph3\DockerArch\Application\Service\CerebroService;
 use Ph3\DockerArch\Application\Service\ElasticsearchHeadService;
 use Ph3\DockerArch\Domain\DockerContainer\Model\DockerContainer;
@@ -40,6 +41,7 @@ class ElasticsearchDockerContainer extends DockerContainer
     public function execute(): void
     {
         $service = $this->getService();
+        $project = $service->getProject();
 
         $this->setFrom(sprintf('blacktop/elasticsearch:%s', $service->getOptions()['version']));
 
@@ -51,6 +53,17 @@ class ElasticsearchDockerContainer extends DockerContainer
                     'echo "http.cors.allow-origin: \\"*\\"" >> /usr/share/elasticsearch/config/elasticsearch.yml'
                 );
         }
+
+        $project
+            ->addEnv('ELASTIC_SEARCH_DATA_LOCATION', Architect::GLOBAL_ABSOLUTE_TMP_DIRECTORY.'/data/elasticsearch');
+
+        // Volumes.
+        $this
+            ->addVolume([
+                'local' => '${'.$project->generateEnvKey('ELASTIC_SEARCH_DATA_LOCATION').'}',
+                'remote' => '/usr/share/elasticsearch/data',
+                'type' => 'rw',
+            ]);
 
         // Ports.
         $this->addEnvPort('ELASTIC_SEARCH', ['from' => '8020', 'to' => '9200']);
