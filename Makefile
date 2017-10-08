5e23eab286b08538473bddfc9604e29a5a9a3a86
Cédric Dugat <cedric@dugat.me>
2017-10-08 23:56:27 +0200

    CapistranoService added

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ test:
 clean-examples:
 	@find examples -type d -name ".docker-arch" | xargs rm -rf
 	@find examples -type d -name "vendor" | xargs rm -rf
+	@find examples -type d -name ".bundle" | xargs rm -rf
+	@find examples -type d -name "log" | xargs rm -rf
+	@find examples -type d -name "logs" | xargs rm -rf
---
+++ src/Application/Service/CapistranoService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ph3\DockerArch\Application\Service;
+
+use Ph3\DockerArch\Domain\Service\Model\AbstractService;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Cédric Dugat <cedric@dugat.me>
+ */
+class CapistranoService extends AbstractService
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptionsResolver(): Options
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setRequired(['version']);
+        $resolver->setDefaults([
+            'version' => '3.9.1',
+            'zsh' => true,
+            'dotfiles' => true,
+        ]);
+
+        return $resolver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCliOnly(): bool
+    {
+        return true;
+    }
+}
